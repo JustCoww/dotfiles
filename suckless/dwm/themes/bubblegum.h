55cd7807c23951f9766b89e24b7d39fe65a52a2c
justcow <68345611+JustCoww@users.noreply.github.com>
2022-03-12 20:05:06 +0000

    Delete suckless directory

--- suckless/dwm/themes/bubblegum.h
+++
@@ -1,4 +0,0 @@
-static const char black[]       = "#0f1114";
-static const char blue[]        = "#55CDFC";  // focused window border
-static const char white[]      = "#FFFFFF";
-static const char pink[]        = "#F7A8B8";

b9565dec3142e301047b11994a7d3205f2554e53
justcow <68345611+JustCoww@users.noreply.github.com>
2022-03-12 20:02:59 +0000

    Add files via upload

---
+++ .suckless/dmenu/themes/bubblegum.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#cbe0eb", "#0f1114" }, /* azul claro; preto */
+	[SchemeSel] = { "#1f2122", "#F7A8B8" }, /* azul claro; azul forte */
+	[SchemeOut] = { "#cbe0eb", "#FFFFFF" }, /* azul claro; azul suave */
+};

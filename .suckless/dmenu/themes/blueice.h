4642abca6ddd657d27d3aeda97d2fbbc93962297
justcow <68345611+JustCoww@users.noreply.github.com>
2022-04-12 19:50:11 +0100

    Add files via upload

---
+++ .suckless/dmenu/themes/blueice.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#e4e6eb", "#0f1114" },
+	[SchemeSel] = { "#0f1114", "#A7BBD0" },
+	[SchemeOut] = { "#e4e6eb", "#C7D9EC" },
+};

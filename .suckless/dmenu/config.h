ad750f8347b5dbdcb0608dee7913bacd0b0a6463
justcow <68345611+JustCoww@users.noreply.github.com>
2022-05-07 23:39:41 +0100

    Add files via upload

--- .suckless/dmenu/config.h
+++ .suckless/dmenu/config.h
@@ -3,12 +3,10 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-	"Fira Mono Font:size=17"
-};
+static const char *fonts[] = {"Fira Mono:size=16"};
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
-#include "themes/blueice.h"
+#include "themes/moz.h"
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;

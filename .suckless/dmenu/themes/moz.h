fb108edda3fd27643d9e9fad523d6da69b74b4f9
justcow <68345611+JustCoww@users.noreply.github.com>
2022-05-07 23:39:23 +0100

    Add files via upload

---
+++ .suckless/dmenu/themes/moz.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#8c88a6", "#1c1b22" },
+	[SchemeSel] = { "#d7d1ff", "#1c1b22" }, 
+	[SchemeOut] = { "#cbe0eb", "#FFFFFF" },
+};
